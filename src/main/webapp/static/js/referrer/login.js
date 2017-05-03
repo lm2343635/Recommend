46bd410129687367dcaf7e7920b82bf21d8173a4
Meng Li <lm2343635@126.com>
2017-05-04 01:11:53 +0900

    Give tip when username and password is empty for refereer and worker.

--- src/main/webapp/static/js/referrer/login.js
+++ src/main/webapp/static/js/referrer/login.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
     $("#login-submit").click(function () {
         var telephone = $("#login-telephone").val();
         var password = $("#login-password").val();
+        if (telephone == "" || password == "") {
+            weui.alert("请输入用户名和密码！");
+            return;
+        }
         ReferrerManager.login(telephone, password, function (success) {
             if (success) {
                 location.href = "orders.html";
--- src/main/webapp/static/js/worker/login.js
+++ src/main/webapp/static/js/worker/login.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
     $("#login-submit").click(function () {
         var number = $("#login-number").val();
         var password = $("#login-password").val();
+        if (number == "" || password == "") {
+            weui.alert("请输入用户名和密码！");
+            return;
+        }
         WorkerManager.login(number, password, function (success) {
             if (success) {
                 location.href = "tasks.html";
